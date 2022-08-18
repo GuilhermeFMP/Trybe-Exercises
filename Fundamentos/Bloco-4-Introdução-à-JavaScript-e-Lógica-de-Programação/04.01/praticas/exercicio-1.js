f8f0b20b830452f28b172d3029a0bfa04507d1b9
Guilherme Pinho <guilhermemoraispinho@gmail.com>
2022-08-18 18:56:56 -0300

    pratica 1.

---
+++ Fundamentos/Bloco-4-Introdução-à-JavaScript-e-Lógica-de-Programação/04.01/praticas/exercicio-1.js
@@ -0,0 +1,14 @@
+const a = 8;
+const b = 5;
+let resultado = 0;
+
+resultado = a + b;
+console.log(resultado);
+resultado = a - b;
+console.log(resultado);
+resultado = a * b;
+console.log(resultado);
+resultado = a / b;
+console.log(resultado);
+resultado = a % b;
+console.log(resultado);
