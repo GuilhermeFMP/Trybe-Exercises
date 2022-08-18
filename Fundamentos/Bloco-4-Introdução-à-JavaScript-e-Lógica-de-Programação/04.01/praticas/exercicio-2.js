4515d0f74bf55adee9b4c8af2deb9679f0e8d30e
Guilherme Pinho <guilhermemoraispinho@gmail.com>
2022-08-18 19:01:39 -0300

    Pratica 2.

---
+++ Fundamentos/Bloco-4-Introdução-à-JavaScript-e-Lógica-de-Programação/04.01/praticas/exercicio-2.js
@@ -0,0 +1,10 @@
+const a = 8;
+const b = 5;
+
+if (a > b) {
+    console.log("O maior número é", a);
+} else if (a < b) {
+    console.log("O maior número é", b);
+} else {
+    console.log("Eles são iguais");
+}
