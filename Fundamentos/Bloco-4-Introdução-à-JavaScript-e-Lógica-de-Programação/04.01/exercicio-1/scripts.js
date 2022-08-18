07f271dc617705a693f97b097cb14ecb1dc64b9f
Guilherme Pinho <guilhermemoraispinho@gmail.com>
2022-08-18 15:17:34 -0300

    Exercicio 1 feito

---
+++ Fundamentos/Bloco-4-Introdução-à-JavaScript-e-Lógica-de-Programação/04.01/exercicio-1/scripts.js
@@ -0,0 +1,11 @@
+let myName = 'Guilherme';
+const birthCity = 'Juquia';
+let birthYear = 1998;
+
+birthYear = 2030
+birthCity = 'Registro'
+
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
