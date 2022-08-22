fb51ade983365814037540b03db70589e2f2d2e4
Guilherme Pinho <guilhermemoraispinho@gmail.com>
2022-08-22 19:59:01 -0300

    Bonus 2

---
+++ Fundamentos/Bloco-4-Introdução-à-JavaScript-e-Lógica-de-Programação/04.03/Prática/bonus-2.js
@@ -0,0 +1,14 @@
+function quadrado(numero) {
+
+let asteriscos = '';
+
+for (let index = 1; index <= numero; index += 1) {
+    asteriscos = asteriscos + '*';
+    
+    console.log(asteriscos);
+}
+    
+    
+}
+    
+quadrado(5);
