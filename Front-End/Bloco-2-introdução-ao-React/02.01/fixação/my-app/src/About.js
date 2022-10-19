6827edde71e125bcd576d91e4a09adba487a7d2c
Guilherme Pinho <guilhermemoraispinho@gmail.com>
2022-10-19 16:28:42 -0300

    Exercicio de fixação

---
+++ Front-End/Bloco-2-introdução-ao-React/02.01/fixação/my-app/src/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class About extends React.Component {
+    render() {
+        return (
+        <main>
+            <div>
+                <h1>Guilherme Pinho</h1>
+                <p>Faço programação na Trybe</p>
+            </div>
+            <div>
+                <h2>Minhas Habilidades</h2>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JAVASCRIPT</li>
+                </ul>
+            </div>
+        </main>
+        );
+      }
+}
+
+export default About;
