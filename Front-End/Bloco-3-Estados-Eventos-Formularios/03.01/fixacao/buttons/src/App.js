ce8fc261365ac32aa9be3c41319dd18f63251ab1
Guilherme Pinho <guilhermemoraispinho@gmail.com>
2022-10-24 16:12:43 -0300

    Fixacao 2

--- Front-End/Bloco-3-Estados-Eventos-Formularios/03.01/fixacao/buttons/src/App.js
+++ Front-End/Bloco-3-Estados-Eventos-Formularios/03.01/fixacao/buttons/src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
 
-function handleClickOne() {
-  console.log('Clicou 1')
-}
-
-function handleClickTwo() {
-  console.log('Clicou 2')
-}
-
-function handleClickThree() {
-  console.log('Clicou 3')
-}
-
 class App extends React.Component {
+  /* constructor() {
+    super();
+    this.handleClickOne = this.handleClickOne.bind(this);
+    this.handleClickTwo = this.handleClickTwo.bind(this);
+    this.handleClickThree = this.handleClickThree.bind(this);
+  } */
+
+  handleClickOne = () => {
+    console.log('Clicou 1', this)
+  }
+  
+  handleClickTwo = () => {
+    console.log('Clicou 2', this)
+  }
+  
+  handleClickThree = () => {
+    console.log('Clicou 3', this)
+  }
+  
   render() {
     return (
       <div className='button'>
-        <button onClick={handleClickOne}>Botão 1</button>
-        <button onClick={handleClickTwo}>Botão 2</button>
-        <button onClick={handleClickThree}>Botão 3</button>
+        <button onClick={this.handleClickOne}>Botão 1</button>
+        <button onClick={this.handleClickTwo}>Botão 2</button>
+        <button onClick={this.handleClickThree}>Botão 3</button>
       </div>
     );
   }
